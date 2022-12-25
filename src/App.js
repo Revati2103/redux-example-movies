8b30de5b64a12411978d5ecb765a1ac6c1ba6997
Revati <revati.v.bordawekar@gmail.com>
2022-12-25 14:39:42 -0800

    completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
-import './App.css';
-
+import { increment, decrement } from "./actions";
+import { useSelector, useDispatch } from "react-redux";
 function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch(); 
   return (
+
+
+
     <div className="App">
-      <h1>Hello react</h1>
+      <h1>Counter : {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {isLogged ? <h3>Info</h3> : '' }
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,20 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {createStore} from 'redux';
+import allReducers from './reducers';
+import { Provider } from 'react-redux';
+
+const store = createStore(allReducers)
 
 
 //STORE -> Global state
 
 //ACTION -> Describes what is going to be done
 
+
+
+
 //REDUCERS -> The actual action that changes the state of the global store
+
+
+
 
 //DISPATCH -> To invoke reducers to change the state of the global store
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
